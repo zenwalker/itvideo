156c01b1d1341717a280bad5a55f3e0ab6f2edb0
Maxim Poletaev <zenwalker2@gmail.com>
2015-07-01 23:01:53 +0300

    Video details and urls fix

--- itvideo/urls.py
+++ itvideo/urls.py
@@ -3,9 +3,10 @@ from django.conf.urls import include, url
 from django.conf import settings
 from django.contrib import admin
 from pages import views as pages
+from videos import views as videos
 
 urlpatterns = [
     url(r'^django/', include(admin.site.urls)),
     url(r'^videos/', include('videos.urls')),
-    url(r'^$', pages.page_home, name='home'),
+    url(r'^$', videos.VideoList.as_view(), name='home'),
 ]
